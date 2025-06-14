e393c517f73eb19f85cd0f13deb568dbf358a654
Matt Coleclough <mattcolecloughfilm@gmail.com>
2025-06-14 13:24:22 +0100

    Configure Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/', // <<< IMPORTANT: Change this to your repository name
+  base: '/', // <<< IMPORTANT: Change this to your repository name
 })
